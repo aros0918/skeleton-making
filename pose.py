b6126fd887c240f89c6c6f3bdd3b975c32fe9ec8
aros0918 <naomifujimura918@gmail.com>
2023-12-10 16:06:58 +0200

    implemented hands

--- pose.py
+++ pose.py
@@ -1,13 +1,102 @@
 import cv2
 import numpy as np
 import math
+import json
+xx = 0
+xy = 0
+xz = 0
+yx = 0
+yy = 0
+yz = 0
+zx = 0
+zy = 0
+zz = 0
+
 def distance(point1, point2):
     return (point1[0] - point2[0])**2 + (point1[1] - point2[1])**2
+def calc_angle(point1, point2):
+    if point2[1] > point1[1] and point2[0] > point1[0]:
+        print("2")
+        return (math.atan((point2[1] - point1[1]) / (point2[0] - point1[0])) / 3.14 * 180)
+    if point2[1] <= point1[1] and point2[0] > point1[0]:
+        
+        print("1")
+        return (math.atan((point2[1] - point1[1]) / (point2[0] - point1[0])) / 3.14 * 180)
+    if point2[1] > point1[1] and point2[0] < point1[0]:
+        print("3")
+        return (180 - math.atan((point2[1] - point1[1]) / (point1[0] - point2[0])) / 3.14 * 180)
+    if point2[1] <= point1[1] and point2[0] <= point1[0]:
+        print("4")
+        return (180 + math.atan((point1[1] - point2[1]) / (point1[0] - point2[0])) / 3.14 * 180)
+def calc_json_values(part, angle):
+    if part == "RightShoulder":
+        print("right")
+        print(angle)
+        with open("bone.json") as file:
+            data = json.load(file)
+        x = -0.075
+        y = 0.067
+        z = math.cos((angle+90)/2/180*3.14)
+        w = math.sqrt(1-x*x-y*y-z*z)
+        xx = 1-2*(y*y+z*z)
+        xy = 2*(x*y - z*w)
+        xz = 2*(x*z+y*w)
+        yx = 2*(x*y+z*w)
+        yy = 1-2*(x*x+z*z)
+        yz = 2*(y*z-x*w)
+        zx = 2*(x*z-y*w)
+        zy = 2*(y*z+x*w)
+        zz = 1-2*(x*x+y*y)
+        for item in data:
+            if item['name'] == 'RightShoulder':
+                transform_mat = item['transform_mat']
+                transform_mat[0][0] = xx
+                transform_mat[0][1] = xy
+                transform_mat[0][2] = xz
+                transform_mat[1][0] = yx
+                transform_mat[1][1] = yy
+                transform_mat[1][2] = yz
+                transform_mat[2][0] = zx
+                transform_mat[2][1] = zy
+                transform_mat[2][2] = zz
+        with open('bone.json', 'w') as file:
+            json.dump(data, file)
+    if part == "LeftShoulder":
+        print("Left")
+        print(angle)
+        with open("bone.json") as file:
+            data = json.load(file)
+        x = -0.075
+        y = -0.068
+        z = math.cos((angle+90)/2/180*3.14)
+        w = math.sqrt(1-x*x-y*y-z*z)
+        xx = 1-2*(y*y+z*z)
+        xy = 2*(x*y - z*w)
+        xz = 2*(x*z+y*w)
+        yx = 2*(x*y+z*w)
+        yy = 1-2*(x*x+z*z)
+        yz = 2*(y*z-x*w)
+        zx = 2*(x*z-y*w)
+        zy = 2*(y*z+x*w)
+        zz = 1-2*(x*x+y*y)
+        for item in data:
+            if item['name'] == 'LeftShoulder':
+                transform_mat = item['transform_mat']
+                transform_mat[0][0] = xx
+                transform_mat[0][1] = xy
+                transform_mat[0][2] = xz
+                transform_mat[1][0] = yx
+                transform_mat[1][1] = yy
+                transform_mat[1][2] = yz
+                transform_mat[2][0] = zx
+                transform_mat[2][1] = zy
+                transform_mat[2][2] = zz
+        with open('bone.json', 'w') as file:
+            json.dump(data, file)
+
 def find_corners(image_path):
     # Read the image
     image = cv2.imread(image_path)
-    
-    
     # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Find corners using the Shi-Tomasi corner detection algorithm
@@ -55,20 +144,11 @@ def find_corners(image_path):
                     direction.add(3)
                 if item[0] < 0 and item[1] < 0:
                     direction.add(4)
-            # Calculate the number of different direction factors
-            print(str(i+1), x, y, len(direction))
             # Draw a circle at the corner location
             if len(direction) != 2:
-              
-              
-              # Add the corner number to the image
               if len(direction) > 2:
-                cv2.circle(image, (x, y), 3, (255, 0, 0), -1)
-                # cv2.putText(image, str(i+1), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
                 body_points.append((x, y))
               else:
-                cv2.circle(image, (x, y), 3, (0, 0, 255), -1)
-                # cv2.putText(image, str(i+1), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
                 other_points.append((x, y))
     if body_points[0][1] < body_points[1][1]:
       upperpoint = body_points[0]
@@ -77,8 +157,10 @@ def find_corners(image_path):
       upperpoint = body_points[1]
       lowerpoint = body_points[0]
     idx = 0
-    print("upperpoint", upperpoint)
-    print("lowerpoint", lowerpoint)
+    # print("upperpoint", upperpoint)
+    # print("lowerpoint", lowerpoint)
+    cv2.circle(image, upperpoint, 3, (255, 0, 0), -1)
+    cv2.circle(image, lowerpoint, 3, (255, 0, 0), -1)
     max = distance(lowerpoint, other_points[0]) - distance(upperpoint, other_points[0])
     for i in range(len(other_points)):
       if max < distance(lowerpoint, other_points[i]) - distance(upperpoint, other_points[i]):
@@ -101,7 +183,8 @@ def find_corners(image_path):
           min = distance(left_top, other_points[i])
           idx = i
     left_arm = other_points[idx]
-    print("left_arm", left_arm)
+    # print("left_arm", left_arm)
+    cv2.circle(image, left_arm, 3, (0, 0, 255), -1)
 
     #left_leg
     idx = 0
@@ -111,7 +194,8 @@ def find_corners(image_path):
           min = distance(left_bottom, other_points[i])
           idx = i
     left_leg = other_points[idx]
-    print("left_leg", left_leg)
+    # print("left_leg", left_leg)
+    cv2.circle(image, left_leg, 3, (0, 0, 255), -1)
 
     #right_arm
     idx = 0
@@ -121,7 +205,8 @@ def find_corners(image_path):
           min = distance(right_top, other_points[i])
           idx = i
     right_arm = other_points[idx]
-    print("right_arm", right_arm)
+    # print("right_arm", right_arm)
+    cv2.circle(image, right_arm, 3, (0, 0, 255), -1)
 
     #right_leg
     idx = 0
@@ -131,36 +216,19 @@ def find_corners(image_path):
           min = distance(right_bottom, other_points[i])
           idx = i
     right_leg = other_points[idx]
-    print("right_leg", right_leg)
-    print("head_point", head_point)
-    #head
+    # print("right_leg", right_leg)
+    cv2.circle(image, right_leg, 3, (0, 0, 255), -1)
 
-    #overlay head images
-    head_img = cv2.imread("data/head.png")
-    print(head_img.shape[0], head_img.shape[1])
-    x1, y1 = head_point[1], head_point[0]
-    x2, y2 = upperpoint[1], upperpoint[0]
-    cv2.circle(image, (y2, x2), 3, (0, 0, 255), -1)
-    angle = math.atan2(y2 - y1, x2 - x1) * 180 / math.pi
-    rows, cols = head_img.shape[:2]
-    M = cv2.getRotationMatrix2D((cols // 2, 0), angle, 1)
-    rotated_head_img = cv2.warpAffine(head_img, M, (cols, rows))
-    image[x2-rows:x2, y2-head_img.shape[1]//2:y2-head_img.shape[1]//2+cols] = rotated_head_img
+    # print("head_point", head_point)
+    cv2.circle(image, head_point, 3, (0, 0, 255), -1)
+   
+    angle_left_arm = calc_angle(left_arm, upperpoint)
+    angle_right_arm = calc_angle(right_arm, upperpoint)
 
-    #overlay body images
-    body_img = cv2.imread("data/body.png")
-    print(body_img.shape[0], body_img.shape[1])
-    x1, y1 = upperpoint[1], upperpoint[0]
-    x2, y2 = lowerpoint[1], lowerpoint[0]
-    cv2.circle(image, (y2, x2), 3, (0, 0, 255), -1)
-    angle = math.atan2(y2 - y1, x2 - x1) * 180 / math.pi
-    rows, cols = body_img.shape[:2]
-    M = cv2.getRotationMatrix2D((cols // 2, 0), angle, 1)
-    rotated_body_img = cv2.warpAffine(body_img, M, (cols, rows))
-    image[x1:x1+rows, y1-body_img.shape[1]//2:y1-body_img.shape[1]//2+cols] = rotated_body_img
+    calc_json_values("RightShoulder", angle_left_arm)
+    calc_json_values("LeftShoulder", angle_right_arm)
+
     cv2.imshow("Final", image)
-
-
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
