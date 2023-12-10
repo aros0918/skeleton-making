2fb91ceec802af6827aec47ac82291a2d1b502c1
aros0918 <naomifujimura918@gmail.com>
2023-12-10 18:06:43 +0200

    final

--- pose.py
+++ pose.py
@@ -19,7 +19,6 @@ def calc_angle(point1, point2):
         print("2")
         return (math.atan((point2[1] - point1[1]) / (point2[0] - point1[0])) / 3.14 * 180)
     if point2[1] <= point1[1] and point2[0] > point1[0]:
-        
         print("1")
         return (math.atan((point2[1] - point1[1]) / (point2[0] - point1[0])) / 3.14 * 180)
     if point2[1] > point1[1] and point2[0] < point1[0]:
@@ -30,8 +29,6 @@ def calc_angle(point1, point2):
         return (180 + math.atan((point1[1] - point2[1]) / (point1[0] - point2[0])) / 3.14 * 180)
 def calc_json_values(part, angle):
     if part == "RightShoulder":
-        print("right")
-        print(angle)
         with open("bone.json") as file:
             data = json.load(file)
         x = -0.075
@@ -62,8 +59,6 @@ def calc_json_values(part, angle):
         with open('bone.json', 'w') as file:
             json.dump(data, file)
     if part == "LeftShoulder":
-        print("Left")
-        print(angle)
         with open("bone.json") as file:
             data = json.load(file)
         x = -0.075
@@ -93,6 +88,91 @@ def calc_json_values(part, angle):
                 transform_mat[2][2] = zz
         with open('bone.json', 'w') as file:
             json.dump(data, file)
+    if part == "RHipJoint":
+        with open("bone.json") as file:
+            data = json.load(file)
+        print(angle)
+        initialRHip = [[0.569, -0.023, 0.778, -0.144], [0.586, -0.358, 0.724, 0.058], [0.568, -0.468, 0.628, 0.251],
+                    [0.507, -0.558, 0.476, 0.454], [0.417, -0.606, 0.304, 0.605], [0.292, -0.613, 0.1, 0.726],
+                    [0.144, -0.576, -0.0115, 0.796]
+                    ]
+        if angle <= -90:
+            angle = -89
+        if angle >= 90:
+            angle = 89
+        angle = angle + 90
+        club = int(angle / 30)
+        spare = int(angle % 30)
+        x = initialRHip[club][0] + (initialRHip[club+1][0] - initialRHip[club][0]) / 30 * spare
+        y = initialRHip[club][1] + (initialRHip[club+1][1] - initialRHip[club][1]) / 30 * spare
+        z = initialRHip[club][2] + (initialRHip[club+1][2] - initialRHip[club][2]) / 30 * spare
+        w = initialRHip[club][3] + (initialRHip[club+1][3] - initialRHip[club][3]) / 30 * spare
+        xx = 1-2*(y*y+z*z)
+        xy = 2*(x*y - z*w)
+        xz = 2*(x*z+y*w)
+        yx = 2*(x*y+z*w)
+        yy = 1-2*(x*x+z*z)
+        yz = 2*(y*z-x*w)
+        zx = 2*(x*z-y*w)
+        zy = 2*(y*z+x*w)
+        zz = 1-2*(x*x+y*y)
+        for item in data:
+            if item['name'] == 'RHipJoint':
+                transform_mat = item['transform_mat']
+                transform_mat[0][0] = xx
+                transform_mat[0][1] = xy
+                transform_mat[0][2] = xz
+                transform_mat[1][0] = yx
+                transform_mat[1][1] = yy
+                transform_mat[1][2] = yz
+                transform_mat[2][0] = zx
+                transform_mat[2][1] = zy
+                transform_mat[2][2] = zz
+        with open('bone.json', 'w') as file:
+            json.dump(data, file)
+    if part == "LHipJoint":
+        with open("bone.json") as file:
+            data = json.load(file)
+        print(angle)
+        initialLHip = [[0.15, 0.578, 0.1, 0.794], [0.29, 0.614, -0.09, 0.718], [0.43, 0.602, -0.33, 0.582],
+                    [0.509, 0.556, -0.482, 0.446], [0.568, 0.468, -0.629, 0.253], [0.586, 0.034, -0.738, 0.023],
+                    [0.564, 0.203, -0.782, -0.168]
+                    ]
+        if angle <= 90:
+            angle = 91
+        if angle >= 270:
+            angle = 269
+        angle = angle - 90
+        club = int(angle / 30)
+        spare = int(angle % 30)
+        x = initialLHip[club][0] + (initialLHip[club+1][0] - initialLHip[club][0]) / 30 * spare
+        y = initialLHip[club][1] + (initialLHip[club+1][1] - initialLHip[club][1]) / 30 * spare
+        z = initialLHip[club][2] + (initialLHip[club+1][2] - initialLHip[club][2]) / 30 * spare
+        w = initialLHip[club][3] + (initialLHip[club+1][3] - initialLHip[club][3]) / 30 * spare
+        xx = 1-2*(y*y+z*z)
+        xy = 2*(x*y - z*w)
+        xz = 2*(x*z+y*w)
+        yx = 2*(x*y+z*w)
+        yy = 1-2*(x*x+z*z)
+        yz = 2*(y*z-x*w)
+        zx = 2*(x*z-y*w)
+        zy = 2*(y*z+x*w)
+        zz = 1-2*(x*x+y*y)
+        for item in data:
+            if item['name'] == 'LHipJoint':
+                transform_mat = item['transform_mat']
+                transform_mat[0][0] = xx
+                transform_mat[0][1] = xy
+                transform_mat[0][2] = xz
+                transform_mat[1][0] = yx
+                transform_mat[1][1] = yy
+                transform_mat[1][2] = yz
+                transform_mat[2][0] = zx
+                transform_mat[2][1] = zy
+                transform_mat[2][2] = zz
+        with open('bone.json', 'w') as file:
+            json.dump(data, file)
+        
 
 def find_corners(image_path):
     # Read the image
@@ -100,7 +180,7 @@ def find_corners(image_path):
     # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Find corners using the Shi-Tomasi corner detection algorithm
-    corners = cv2.goodFeaturesToTrack(gray, maxCorners=20, qualityLevel=0.02, minDistance=30)
+    corners = cv2.goodFeaturesToTrack(gray, maxCorners=20, qualityLevel=0.02, minDistance=5)
     # Draw circles at the corner locations on the original image
     body_points = []
     other_points = []
@@ -224,9 +304,13 @@ def find_corners(image_path):
    
     angle_left_arm = calc_angle(left_arm, upperpoint)
     angle_right_arm = calc_angle(right_arm, upperpoint)
-
+    angle_left_leg = calc_angle(left_leg, lowerpoint)
+    angle_right_leg = calc_angle(right_leg, lowerpoint)
+   
     calc_json_values("RightShoulder", angle_left_arm)
     calc_json_values("LeftShoulder", angle_right_arm)
+    calc_json_values("RHipJoint", angle_left_leg)
+    calc_json_values("LHipJoint", angle_right_leg)
 
     cv2.imshow("Final", image)
     cv2.waitKey(0)
